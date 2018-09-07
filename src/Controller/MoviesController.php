a42152b2626c94c77ac8f7c940794ac6a85aac77
mrdang <khacquyet.dang.pro@gmail.com>
2018-09-08 00:20:55 +0200

    post request with param converter ok

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\ControllerTrait;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MoviesController extends AbstractController
@@ -17,5 +19,18 @@ class MoviesController extends AbstractController
     {
         $movies = $this->getDoctrine()->getRepository('App:Movie')->findAll();
         return $movies;
+    }
+
+    /**
+     * @Rest\View(statusCode=201)
+     * @ParamConverter("movie", converter="fos_rest.request_body")
+     * @Rest\NoRoute
+     */
+    public function postMoviesAction(Movie $movie)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($movie);
+        $em->flush();
+        return $movie;
     }
 }
