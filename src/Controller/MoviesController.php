eb52891c19b725c046e8eae8f89c30e2573fd6b6
mrdang <khacquyet.dang.pro@gmail.com>
2018-09-07 23:44:11 +0200

    get movies route ok

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -5,19 +5,17 @@ namespace App\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\ControllerTrait;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
     use ControllerTrait;
 
     /**
-     * @Route("/movies", name="movies")
-     * @Rest\View
+     * @Rest\View()
      */
     public function getMoviesAction()
     {
-        $movies = $this->getDoctrine()->getRepository('App\Entity:Movie')->findAll();
+        $movies = $this->getDoctrine()->getRepository('App:Movie')->findAll();
         return $movies;
     }
 }
